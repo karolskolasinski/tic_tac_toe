8d65ec855c89a7bd21bde19370c8d60453ebfaa4
Karol Skolasiński <karolskolasinski@gmail.com>
2020-03-20 15:54:17 +0100

    BoardController()

--- src/main/java/tic_tac_toe/BoardController.java
+++ src/main/java/tic_tac_toe/BoardController.java
@@ -4,8 +4,14 @@ import java.util.Scanner;
 
 class BoardController {
 
+    private String[][] board = new String[3][3];
+
     void playWithParameters(Scanner scanner, Messages messages, String userSymbol, String computerSymbol, String level) {
-
-
+        boolean gameEnd = false;
+        do {
+            drawBoard();
+            tick();
+            gameEnd = gameStatus();
+        } while (!gameEnd);
     }
 }
