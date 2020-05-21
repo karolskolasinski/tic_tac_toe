c4e9d03c9ebb0e91827dc2c6e9c908ed49436fa1
Karol Skolasiński <karolskolasinski@gmail.com>
2020-05-21 21:56:35 +0200

    add Medium level

--- src/main/java/tic_tac_toe/level/Medium.java
+++ src/main/java/tic_tac_toe/level/Medium.java
@@ -8,10 +8,14 @@ import java.util.Random;
 
 
 public class Medium implements GameLevel {
+
     private Random random = new Random();
+    char[][] board;
 
     @Override
     public void aiMove(char[][] board, char human, char ai, int depth, char turn, GameValidator gameValidator) {
+        this.board = board;
+
         if (!gameValidator.isGameOver(board, human, ai)) {
             List<Cell> availableCells = gameValidator.getAvailableCells(board);
 
@@ -25,8 +29,75 @@ public class Medium implements GameLevel {
                 }
             }
 
+            //attack
+            for (int i = 0; i < 3; i++) {
+                if (board[i][0] == ai && board[i][1] == ai && board[i][2] == ' ') {
+                    board[i][2] = ai;
+                    return;
+                }
+
+                if (board[i][1] == ai && board[i][2] == ai && board[i][0] == ' ') {
+                    board[i][0] = ai;
+                    return;
+                }
+
+                if (board[i][0] == ai && board[i][2] == ai && board[i][1] == ' ') {
+                    board[i][1] = ai;
+                    return;
+                }
+
+                if (board[0][i] == ai && board[1][i] == ai && board[2][i] == ' ') {
+                    board[2][i] = ai;
+                    return;
+                }
+
+                if (board[1][i] == ai && board[2][i] == ai && board[0][i] == ' ') {
+                    board[0][i] = ai;
+                    return;
+                }
+
+                if (board[0][i] == ai && board[2][i] == ai && board[1][i] == ' ') {
+                    board[1][i] = ai;
+                    return;
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (board[i][0] == human && board[i][1] == human && board[i][2] == ' ') {
+                    board[i][2] = ai;
+                    return;
+                }
+
+                if (board[i][1] == human && board[i][2] == human && board[i][0] == ' ') {
+                    board[i][0] = ai;
+                    return;
+                }
+
+                if (board[i][0] == human && board[i][2] == human && board[i][1] == ' ') {
+                    board[i][1] = ai;
+                    return;
+                }
+
+                if (board[0][i] == human && board[1][i] == human && board[2][i] == ' ') {
+                    board[2][i] = ai;
+                    return;
+                }
+
+                if (board[1][i] == human && board[2][i] == human && board[0][i] == ' ') {
+                    board[0][i] = ai;
+                    return;
+                }
+
+                if (board[0][i] == human && board[2][i] == human && board[1][i] == ' ') {
+                    board[1][i] = ai;
+                    return;
+                }
+            }
+
             int i = random.nextInt(availableCells.size());
             gameValidator.placeAMove(board, availableCells.get(i), ai);
         }
     }
+
+
 }
