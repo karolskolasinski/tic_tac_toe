ad9498c1c82fd69c701a5b74a3400b6d9fac4b25
Karol Skolasiński <karolskolasinski@gmail.com>
2020-05-21 22:28:19 +0200

    fix Medium level

--- src/main/java/tic_tac_toe/level/Medium.java
+++ src/main/java/tic_tac_toe/level/Medium.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class Medium implements GameLevel {
 
     private Random random = new Random();
-    char[][] board;
+    private char[][] board;
 
     @Override
     public void aiMove(char[][] board, char human, char ai, int depth, char turn, GameValidator gameValidator) {
@@ -31,72 +31,63 @@ public class Medium implements GameLevel {
 
             //attack
             for (int i = 0; i < 3; i++) {
-                if (board[i][0] == ai && board[i][1] == ai && board[i][2] == ' ') {
-                    board[i][2] = ai;
-                    return;
-                }
-
-                if (board[i][1] == ai && board[i][2] == ai && board[i][0] == ' ') {
-                    board[i][0] = ai;
-                    return;
-                }
-
-                if (board[i][0] == ai && board[i][2] == ai && board[i][1] == ' ') {
-                    board[i][1] = ai;
-                    return;
-                }
-
-                if (board[0][i] == ai && board[1][i] == ai && board[2][i] == ' ') {
-                    board[2][i] = ai;
-                    return;
-                }
-
-                if (board[1][i] == ai && board[2][i] == ai && board[0][i] == ' ') {
-                    board[0][i] = ai;
-                    return;
-                }
-
-                if (board[0][i] == ai && board[2][i] == ai && board[1][i] == ' ') {
-                    board[1][i] = ai;
-                    return;
-                }
+                if (checkHorizontalLines(board, ai, ai, i)) return;
+                if (checkVerticalLines(board, ai, ai, i)) return;
             }
 
+            //defence
             for (int i = 0; i < 3; i++) {
-                if (board[i][0] == human && board[i][1] == human && board[i][2] == ' ') {
-                    board[i][2] = ai;
-                    return;
-                }
-
-                if (board[i][1] == human && board[i][2] == human && board[i][0] == ' ') {
-                    board[i][0] = ai;
-                    return;
-                }
-
-                if (board[i][0] == human && board[i][2] == human && board[i][1] == ' ') {
-                    board[i][1] = ai;
-                    return;
-                }
-
-                if (board[0][i] == human && board[1][i] == human && board[2][i] == ' ') {
-                    board[2][i] = ai;
-                    return;
-                }
-
-                if (board[1][i] == human && board[2][i] == human && board[0][i] == ' ') {
-                    board[0][i] = ai;
-                    return;
-                }
-
-                if (board[0][i] == human && board[2][i] == human && board[1][i] == ' ') {
-                    board[1][i] = ai;
-                    return;
-                }
+                if (checkHorizontalLines(board, human, ai, i)) return;
+                if (checkVerticalLines(board, human, ai, i)) return;
             }
 
             int i = random.nextInt(availableCells.size());
             gameValidator.placeAMove(board, availableCells.get(i), ai);
         }
+    }
+
+    private boolean checkHorizontalLines(char[][] board, char player, char ai, int i) {
+        if (checkLineHorizontal(i, player, 0, 1, 2)) {
+            board[i][2] = ai;
+            return true;
+        }
+
+        if (checkLineHorizontal(i, player, 1, 2, 0)) {
+            board[i][0] = ai;
+            return true;
+        }
+
+        if (checkLineHorizontal(i, player, 0, 2, 1)) {
+            board[i][1] = ai;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkVerticalLines(char[][] board, char player, char ai, int i) {
+        if (checkLineVertical(i, player, 0, 1, 2)) {
+            board[2][i] = ai;
+            return true;
+        }
+
+        if (checkLineVertical(i, player, 1, 2, 0)) {
+            board[0][i] = ai;
+            return true;
+        }
+
+        if (checkLineVertical(i, player, 0, 2, 1)) {
+            board[1][i] = ai;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkLineHorizontal(int i, char player, int one, int two, int three) {
+        return board[i][one] == player && board[i][two] == player && board[i][three] == ' ';
+    }
+
+    private boolean checkLineVertical(int i, char player, int one, int two, int three) {
+        return board[one][i] == player && board[two][i] == player && board[three][i] == ' ';
     }
 
 
